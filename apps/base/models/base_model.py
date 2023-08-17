a9fc72efb5044c95ce54f6f1345455d089573c6c
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2023-08-17 17:51:13 -0500

    change field id of basemodel

--- apps/base/models/base_model.py
+++ apps/base/models/base_model.py
@@ -1,8 +1,9 @@
+import uuid
 from django.db import models
 
 
 class BaseModel(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
